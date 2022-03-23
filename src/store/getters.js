aa0cdb9d82fc505f350391a74f13dd28d3bfa4d6
LJMing <376855074@qq.com>
2022-03-23 23:20:51 +0800

    读取公司角色信息

--- src/store/getters.js
+++ src/store/getters.js
@@ -3,6 +3,7 @@ const getters = {
   device: state => state.app.device,
   token: state =>state.user.token,
   name: state =>state.user.userInfo.username, // 建立用户名称的映射
-  staffPhoto: state =>state.user.userInfo.staffPhoto
+  staffPhoto: state =>state.user.userInfo.staffPhoto,
+  companyId: state => state.user.userInfo.companyId // 建立对于user模块的companyId的快捷访问
 }
 export default getters
--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -45,16 +45,16 @@
           />
           <el-form label-width="120px" style="margin-top:50px">
             <el-form-item label="公司名称">
-              <el-input disabled style="width:400px" />
+              <el-input v-model="formData.name" disabled style="width:400px" />
             </el-form-item>
             <el-form-item label="公司地址">
-              <el-input disabled style="width:400px" />
+              <el-input v-model="formData.companyAddress" disabled style="width:400px" />
             </el-form-item>
             <el-form-item label="邮箱">
-              <el-input disabled style="width:400px" />
+              <el-input v-model="formData.mailbox" disabled style="width:400px" />
             </el-form-item>
             <el-form-item label="备注">
-              <el-input type="textarea" :rows="3" disabled style="width:400px" />
+              <el-input v-model="formData.remarks" type="textarea" :rows="3" disabled style="width:400px" />
             </el-form-item>
           </el-form>
         </el-tab-pane>
@@ -66,7 +66,8 @@
 </template>
 
 <script>
-import {getRoleList} from '@/api/setting'
+import {getRoleList,getCompanyInfo} from '@/api/setting'
+import {mapGetters} from 'vuex'
 export default {
   data(){
     return {
@@ -76,11 +77,18 @@ export default {
         page:1,
         pagesize:10,
         total:0 //记录总数
+      },
+      formData:{
+
       }
     }
   },
+  computed:{
+    ...mapGetters(['companyId'])
+  },
   created(){
     this.getRoleList() 
+    this.getCompanyInfo()
   },
   methods:{
     async getRoleList(){
@@ -93,6 +101,9 @@ export default {
       // newPage是当前点击的页码 
       this.page.page = newPage // 将当前页码赋值给当前的最新页码
       this.getRoleList()
+    },
+    async getCompanyInfo(){
+      this.formData = await getCompanyInfo(this.companyId)
     }
   }
 }
