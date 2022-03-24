1981c0a145f35e4bf705046c385d75f9b94e275d
LJMing <376855074@qq.com>
2022-03-24 21:20:55 +0800

    完成角色编辑功能

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -21,7 +21,7 @@
             <el-table-column align="center" label="操作">
               <template slot-scope="{row}">
                 <el-button size="small" type="success">分配权限</el-button>
-                <el-button size="small" type="primary">编辑</el-button>
+                <el-button size="small" type="primary" @click="editRole(row.id)">编辑</el-button>
                 <el-button size="small" type="danger" @click="deleteRole(row.id)">删除</el-button>
               </template>
 
@@ -65,11 +65,29 @@
       </el-tabs>
     </el-card>
   </div>
+  <!-- 弹层 -->
+  <el-dialog title="编辑弹层" :visible="showDialog">
+    <el-form ref="roleForm" :model="roleForm" :rules="rules" label-width="120px" >
+      <el-form-item style="width:80%" label="角色名称" prop="name">
+        <el-input v-model="roleForm.name"></el-input>
+      </el-form-item>
+      <el-form-item style="width:80%" label="角色描述">
+        <el-input v-model="roleForm.description"></el-input>
+      </el-form-item>
+    </el-form>
+    <!-- 底部 -->
+    <el-row slot="footer" type="flex" justify="center">
+      <el-col :span="18" class="dialog-footer">
+        <el-button size="small" @click="btnCancel">取消</el-button>
+        <el-button size="small" type="primary" @click="btnOK">确定</el-button>
+      </el-col>
+    </el-row>
+  </el-dialog>
 </div>
 </template>
 
 <script>
-import {getRoleList,getCompanyInfo,deleteRole} from '@/api/setting'
+import {getRoleList,getCompanyInfo,deleteRole, getRoleDetail} from '@/api/setting'
 import {mapGetters} from 'vuex'
 export default {
   data(){
@@ -81,9 +99,12 @@ export default {
         pagesize:10,
         total:0 //记录总数
       },
-      formData:{
-
-      }
+      formData:{},
+      showDialog:false,
+      roleForm:{},
+      rules:{
+        name:[{required:true,$message:'角色名称不能为空',trigger: 'blur'}]
+      },
     }
   },
   computed:{
@@ -119,6 +140,28 @@ export default {
       }catch(error){
         console.log(error)
       }
+    },
+    async editRole(id){
+      this.roleForm = await getRoleDetail(id)
+      this.showDialog = true // 为了不出现闪烁的问题 先获取数据 再弹出层
+      
+    },
+    async btnOK(){
+      try{
+        await this.$refs.roleForm.validate()
+        // 只有校验通过的情况下 才会执行await的下方内容
+        // roleForm这个对象有id就是编辑 没有id就是新增
+
+        // 重新拉取数据
+        this.getRoleList()
+        this.$message.success('操作成功')
+        this.showDialog = false
+      }catch(error){
+        console.log(error)
+      }
+    },
+    btnCancel(){
+      
     }
   }
 }
